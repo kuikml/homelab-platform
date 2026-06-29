d0dc0f78f574707f88a80314b21d81ff3417ffa7
kuikml <kuikbartlomiej@gmail.com>
2026-06-29 21:05:32 +0200

    #Terraform -Setting up proxmox provider

---
+++ terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    proxmox = {
+        source = "telmate/proxmox"
+        version = "3.0.1-rc4"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = var.pm_api_url
+  pm_api_token_id = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure = true
+
+  pm_log_enable = true
+  pm_log_file   = "terraform-proxmox.log"
+}
