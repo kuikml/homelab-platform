700c786ad51cf56fc13f2c6e0ba6b20c24bad2ed
Bartek <kuikbartlomiej@gmail.com>
2026-08-15 20:24:27 +0000

    fix(terraform): escape git config quotes and fix tfvars syntax

--- terraform/bastion/providers.tf
+++ terraform/bastion/providers.tf
@@ -2,17 +2,17 @@ terraform {
   required_version = ">= 0.14"
   required_providers {
     proxmox = {
-        source = "telmate/proxmox"
-        version = "3.0.2-rc04"
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc04"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url      = var.pm_api_url
-  pm_api_token_id = var.pm_api_token_id
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
-  pm_tls_insecure = true
+  pm_tls_insecure     = true
 
   pm_log_enable = true
   pm_log_file   = "terraform-proxmox.log"
